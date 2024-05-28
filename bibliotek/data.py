b56b0d1c69f4b6f6c9a0202aabfe335a3d8a1a38
tp <tobias.gamme@gmail.com>
2024-05-28 15:03:01 +0200

    la til en if len

--- bibliotek/data.py
+++ bibliotek/data.py
@@ -6,7 +6,6 @@ cur = con.cursor()
 
 
 cur.execute("""CREATE TABLE IF NOT EXISTS bøker(
-            ID INTEGER PRIMARY KEY,
             tittel TEXT,
             forfatter TEXT,
             isbn INTEGER,
@@ -18,17 +17,18 @@ con.commit()
 cur.executemany("INSERT INTO bøker(nummer) VALUES(?)", [(nummer,) for nummer in range(1,52)])
 
 booklist = []
-with open("./bøker.csv", "r", encoding="utf-8") as file:
+with open("C:\\Users\\tobia\\Desktop\\eksamen\\bibliotek\\bøker.csv", "r", encoding="utf-8") as file:
     bøker = [book for book in file.read().split("\n")]
     for book in bøker:
         d_book = book.split(",")
-        booklist.append(
-            { "tittel": d_book[0],
-              "forfatter": d_book[1],
-              "isbn": d_book[2],
-              "nummer": d_book[3]
-            }
-        )
+        if len(d_book) >=4:
+            booklist.append(
+                { "tittel": d_book[0],
+                "forfatter": d_book[1],
+                "isbn": d_book[2],
+                "nummer": d_book[3]
+                }
+            )
 
 cur.executemany("UPDATE bøker SET tittel = ?, forfatter = ?, isbn = ? WHERE nummer = ?",
                 [(book["tittel"], book["forfatter"], book["isbn"], book["nummer"]) for book in booklist],)
